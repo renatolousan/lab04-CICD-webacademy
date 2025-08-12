5e6e1a568a47d61e178a56da56ece3dd82321863
Renato Lousan <renatolousan@hotmail.com>
2025-08-11 20:01:40 -0400

    add as credenciais do docker

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Bem vindos ao WebAcademy!!');
+  res.send('Bem vindos ao WebAcademy!!!');
 });
 
 app.listen(8080, () => {
